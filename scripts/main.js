dcd952a611aadd45a64e5222312a0b713c4ce2ad
Bas Visser <156429113+debasvisser@users.noreply.github.com>
2025-04-04 11:53:53 +0200

    opdrachten af

--- scripts/main.js
+++ scripts/main.js
@@ -106,16 +106,80 @@ console.log(departmentChoice);
 
 switch (departmentChoice) {
     case 'marketing':
-        console.log(`je koos ${departmentChoice}. ${departments.marketing.description}`);
+        console.log(`Je koos ${departmentChoice}. ${departments.marketing.description}`);
         break;
     case 'sales':
-        console.log(`je koos ${departmentChoice}. ${departments.sales.description}`);
+        console.log(`Je koos ${departmentChoice}. ${departments.sales.description}`);
         break;
     case 'customer-service':
-        console.log(`je koos ${departmentChoice}. ${departments['customer-service'].description}`);
+        console.log(`Je koos ${departmentChoice}. ${departments['customer-service'].description}`);
         break;
     default:
         console.error(`Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.`);
         break;
 }
 
+// Opdracht 3a //
+
+// const functionChoice = prompt(`Je koos marketing. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in.\n` +
+//     `0: ${departments.marketing.jobs[0].title},\n` +
+//     `1: ${departments.marketing.jobs[1].title},\n` +
+//     `2: ${departments.marketing.jobs[2].title},\n` +
+//     `3: ${departments.marketing.jobs[3].title}`);
+//
+// console.log(functionChoice);
+
+// Opdracht 3b + 3c //
+
+// switch (functionChoice) {
+//     case '0':
+//         console.log(`Je koos ${departments.marketing.jobs[0].title}. Een uitdagende rol! ${departments.marketing.jobs[0].description}`);
+//         break;
+//     case '1':
+//         console.log(`Je koos ${departments.marketing.jobs[1].title}. Een uitdagende rol! ${departments.marketing.jobs[1].description}`);
+//         break;
+//     case '2':
+//         console.log(`Je koos ${departments.marketing.jobs[2].title}. Een uitdagende rol! ${departments.marketing.jobs[2].description}`);
+//         break;
+//     case '3':
+//         console.log(`Je koos ${departments.marketing.jobs[3].title}. Een uitdagende rol! ${departments.marketing.jobs[3].description}`);
+//         break;
+//     default:
+//         console.error('Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.');
+// }
+
+// Opdracht 4a //
+
+console.log(`${departmentChoice} is een leuke afdeling om te werken. Er werken op de moment ${departments[departmentChoice].numberOfEmployees} medewerkers.`)
+
+// Opdracht 4b //
+
+const functionChoice = prompt(`Je koos ${departmentChoice}. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in.\n`
+    + `0: ${departments[departmentChoice].jobs[0].title}\n`
+    + `1: ${departments[departmentChoice].jobs[1].title}\n`
+    + `2: ${departments[departmentChoice].jobs[2].title}\n`
+    + `3: ${departments[departmentChoice].jobs[3].title}`
+);
+
+    console.log(functionChoice);
+
+switch (functionChoice) {
+    case '0':
+        console.log(`Je koos ${departments[departmentChoice].jobs[0].title}. Een uitdagende rol! ${departments[departmentChoice].jobs[0].title}`);
+        break;
+    case '1':
+        console.log(`Je koos ${departments[departmentChoice].jobs[1].title}. Een uitdagende rol! ${departments[departmentChoice].jobs[1].description}`);
+        break;
+    case '2':
+        console.log(`Je koos ${departments[departmentChoice].jobs[2].title}. Een uitdagende rol! ${departments[departmentChoice].jobs[2].description}`);
+        break;
+    case '3':
+        console.log(`Je koos ${departments[departmentChoice].jobs[3].title}. Een uitdagende rol! ${departments[departmentChoice].jobs[3].description}`);
+        break;
+    default:
+        console.log('Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.');
+        document.getElementById('error-message').textContent = 'Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.';
+}
+
+// document.getElementById('role-title').textContent = 'Bas!';
+// document.getElementById('department-description').textContent = 'lasagna';
